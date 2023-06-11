13020269c9bb252436aa44cc13021c2564b46dcd
HaimSlbah <26life72@gmail.com>
2023-06-11 10:44:51 +0300

    I successfully finished the back

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 require("dotenv/config");
 const express = require("express");
 const mongooseConnection = require("./data/mongoConnect")
+const cors = require("cors");
+const pinsRouter = require("./routes/pin.routes");
+const usersRouter = require("./routes/users.routes");
 
 const app = express();
+app.use(express.json());
+app.use(cors("*"));
+app.use("/api/pins", pinsRouter);
+app.use("/api/users", usersRouter);
+
 
 
 mongooseConnection()
---
+++ backend/models/Pin.js
@@ -0,0 +1,39 @@
+const {model, Schema} = require("mongoose");
+
+const PinSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        min: 3,
+    },
+    desc: {
+        type: String,
+        required: true,
+        min: 3,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5,
+    },
+    lat: {
+        type: Number,
+        required:true,
+    },
+    long: {
+        type: Number,
+        required:true,
+    }
+
+},{timestamps: true}
+);
+
+module.exports = model("Pin",PinSchema);
+
+
+
